ec85f1bb61ae4def7ca75562d007ff15f3e420be
Celia CORSIN <ccorsin@student.42.fr>
2019-04-12 16:50:39 +0200

    Add all celia

--- pablo.sh
+++ pablo.sh
@@ -1,16 +1,221 @@
-echo "\nand_conc_2"
-python3 main.py pablo/and_conc_2.txt
-echo "\nand_conc_3"
-python3 main.py pablo/and_conc_3.txt
-echo "\nconc_same_2.txt"
-python3 main.py pablo/conc_same_2.txt
-echo "\nconc_same_4.txt"
-python3 main.py pablo/conc_same_4.txt
-echo "\nor 5"
+echo "---------------------- AND ------------------ \n\n"
+python3 main.py pablo/and_0.txt
+echo "----"
+python3 main.py pablo/and_1.txt
+echo "----"
+python3 main.py pablo/and_2.txt
+echo "----"
+python3 main.py pablo/and_3.txt
+echo "----"
+python3 main.py pablo/and_4.txt
+echo "----"
+python3 main.py pablo/and_5.txt
+
+echo "---------------------- AND CCL ------------------ \n\n"
+python3 main.py pablo/and_conclusion_0.txt
+echo "----"
+python3 main.py pablo/and_conclusion_1.txt
+echo "----"
+python3 main.py pablo/and_conclusion_2.txt
+echo "----"
+python3 main.py pablo/and_conclusion_3.txt
+
+
+echo "---------------------- SAME FACT ------------------ \n\n"
+python3 main.py pablo/conclusion_same_fact_0.txt
+echo "----"
+python3 main.py pablo/conclusion_same_fact_1.txt
+echo "----"
+python3 main.py pablo/conclusion_same_fact_2.txt
+echo "----"
+python3 main.py pablo/conclusion_same_fact_3.txt
+echo "----"
+python3 main.py pablo/conclusion_same_fact_4.txt
+echo "----"
+python3 main.py pablo/conclusion_same_fact_5.txt
+
+echo "---------------------- EVAL ------------------ \n\n"
+python3 main.py pablo/eval_0.txt
+echo "----"
+python3 main.py pablo/eval_1.txt
+echo "----"
+python3 main.py pablo/eval_10.txt
+echo "----"
+python3 main.py pablo/eval_11.txt
+echo "----"
+python3 main.py pablo/eval_12.txt
+echo "----"
+python3 main.py pablo/eval_13.txt
+echo "----"
+python3 main.py pablo/eval_14.txt
+echo "----"
+python3 main.py pablo/eval_15.txt
+echo "----"
+python3 main.py pablo/eval_16.txt
+echo "----"
+python3 main.py pablo/eval_17.txt
+echo "----"
+python3 main.py pablo/eval_18.txt
+echo "----"
+python3 main.py pablo/eval_19.txt
+echo "----"
+python3 main.py pablo/eval_2.txt
+echo "----"
+python3 main.py pablo/eval_20.txt
+echo "----"
+python3 main.py pablo/eval_21.txt
+echo "----"
+python3 main.py pablo/eval_22.txt
+echo "----"
+python3 main.py pablo/eval_23.txt
+echo "----"
+python3 main.py pablo/eval_24.txt
+echo "----"
+python3 main.py pablo/eval_25.txt
+echo "----"
+python3 main.py pablo/eval_26.txt
+echo "----"
+python3 main.py pablo/eval_27.txt
+echo "----"
+python3 main.py pablo/eval_28.txt
+echo "----"
+python3 main.py pablo/eval_3.txt
+echo "----"
+python3 main.py pablo/eval_4.txt
+echo "----"
+python3 main.py pablo/eval_5.txt
+echo "----"
+python3 main.py pablo/eval_6.txt
+echo "----"
+python3 main.py pablo/eval_7.txt
+echo "----"
+python3 main.py pablo/eval_8.txt
+echo "----"
+python3 main.py pablo/eval_9.txt
+
+echo "---------------------- IMPLY ------------------ \n\n"
+python3 main.py pablo/imply_0.txt
+echo "----"
+python3 main.py pablo/imply_1.txt
+echo "----"
+python3 main.py pablo/imply_2.txt
+echo "----"
+python3 main.py pablo/imply_3.txt
+
+
+echo "---------------------- NOT ------------------ \n\n"
+python3 main.py pablo/not_0.txt
+echo "----"
+python3 main.py pablo/not_1.txt
+echo "----"
+python3 main.py pablo/not_2.txt
+echo "----"
+python3 main.py pablo/not_3.txt
+echo "----"
+python3 main.py pablo/not_4.txt
+echo "----"
+python3 main.py pablo/not_5.txt
+echo "----"
+python3 main.py pablo/not_6.txt
+echo "----"
+python3 main.py pablo/not_7.txt
+echo "----"
+python3 main.py pablo/not_8.txt
+
+
+echo "---------------------- OR ------------------ \n\n"
+python3 main.py pablo/or_0.txt
+echo "----"
+python3 main.py pablo/or_1.txt
+echo "----"
+python3 main.py pablo/or_2.txt
+echo "----"
+python3 main.py pablo/or_3.txt
+echo "----"
+python3 main.py pablo/or_4.txt
+echo "----"
 python3 main.py pablo/or_5.txt
-echo "\nprio 3"
-python3 main.py pablo/prio_3.txt
-echo "\nprio 1"
+
+echo "---------------------- PRIORITY ------------------ \n\n"
+python3 main.py pablo/priority_0.txt
+echo "----"
 python3 main.py pablo/priority_1.txt
-echo "\nxor_5"
+echo "----"
+python3 main.py pablo/priority_2.txt
+echo "----"
+python3 main.py pablo/priority_3.txt
+echo "----"
+python3 main.py pablo/priority_4.txt
+echo "----"
+
+echo "---------------------- XOR ------------------ \n\n"
+python3 main.py pablo/xor_0.txt
+echo "----"
+python3 main.py pablo/xor_1.txt
+echo "----"
+python3 main.py pablo/xor_2.txt
+echo "----"
+python3 main.py pablo/xor_3.txt
+echo "----"
+python3 main.py pablo/xor_4.txt
+echo "----"
 python3 main.py pablo/xor_5.txt
+
+echo "---------------------- EROOR ------------------ \n\n"
+python3 main.py pablo/eval_0.txt
+echo "----"
+python3 main.py pablo/eval_1.txt
+echo "----"
+python3 main.py pablo/eval_10.txt
+echo "----"
+python3 main.py pablo/eval_11.txt
+echo "----"
+python3 main.py pablo/eval_12.txt
+echo "----"
+python3 main.py pablo/eval_13.txt
+echo "----"
+python3 main.py pablo/eval_14.txt
+echo "----"
+python3 main.py pablo/eval_15.txt
+echo "----"
+python3 main.py pablo/eval_16.txt
+echo "----"
+python3 main.py pablo/eval_17.txt
+echo "----"
+python3 main.py pablo/eval_18.txt
+echo "----"
+python3 main.py pablo/eval_19.txt
+echo "----"
+python3 main.py pablo/eval_2.txt
+echo "----"
+python3 main.py pablo/eval_20.txt
+echo "----"
+python3 main.py pablo/eval_21.txt
+echo "----"
+python3 main.py pablo/eval_22.txt
+echo "----"
+python3 main.py pablo/eval_23.txt
+echo "----"
+python3 main.py pablo/eval_24.txt
+echo "----"
+python3 main.py pablo/eval_25.txt
+echo "----"
+python3 main.py pablo/eval_26.txt
+echo "----"
+python3 main.py pablo/eval_27.txt
+echo "----"
+python3 main.py pablo/eval_28.txt
+echo "----"
+python3 main.py pablo/eval_3.txt
+echo "----"
+python3 main.py pablo/eval_4.txt
+echo "----"
+python3 main.py pablo/eval_5.txt
+echo "----"
+python3 main.py pablo/eval_6.txt
+echo "----"
+python3 main.py pablo/eval_7.txt
+echo "----"
+python3 main.py pablo/eval_8.txt
+echo "----"
+python3 main.py pablo/eval_9.txt
